634e2e9370e919fd131a1519c4bd3ef03ca68dc0
Md. Golam Rabby Jim <jim15-5551@diu.bd>
2020-11-14 20:11:24 +0600

    deployment bug fixed

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,10 +1,7 @@
 from django.urls import path
-from django.contrib import admin
 from .views import SignUpView, LoginView
 
 urlpatterns = [
-    # path('login/', login),
-    path('admin/', admin.site.urls),
     path('login/', LoginView.as_view(), name='login'),
     path('register/', SignUpView.as_view(), name='register'),
 ]
--- grayspaceit/settings.py
+++ grayspaceit/settings.py
@@ -117,7 +117,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
-    'DEFAULT_AUTHENTICATION_CLASSES': [  # new
+    'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ],
